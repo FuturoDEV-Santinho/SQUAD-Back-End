c0e32c733c155a36f194a74ff782b9195cb6db4f
BerCad <bernardo.c1995@aluno.ifsc.edu.org.br>
2023-06-20 23:38:28 -0300

    Criado o repositories para o Usuário.

--- src/main/java/com/devinadotion/projeto/repositories/UsuarioRepository.java
+++ src/main/java/com/devinadotion/projeto/repositories/UsuarioRepository.java
@@ -4,7 +4,5 @@ import com.devinadotion.projeto.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
     Usuario findByEmail(String email);
-
 }
